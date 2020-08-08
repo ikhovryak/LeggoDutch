6edf11e545d9527b4b081ca1d63283c93d24d65b
Samuel <samueltan97@hotmail.com>
2020-08-08 00:10:44 -0400

    Added image processing scripts

---
+++ image-processing/generic-open-cv/image-processing.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import mapper
+image=cv2.imread("demo_image/receipt_00700.png")   #read in the image
+image=cv2.resize(image,(960,1280)) #resizing because opencv does not work well with bigger images
+orig=image.copy()
+
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+# cv2.imshow("Title",gray)
+
+blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+# cv2.imshow("Blur",blurred)
+
+edged=cv2.Canny(blurred,30,50)  #30 MinThreshold and 50 is the MaxThreshold
+# cv2.imshow("Canny",edged)
+cv2.imwrite('images/processed_image.jpg', edged)
+
+
+_,contours,_=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  #retrieve the contours as a list, with simple apprximation model
+contours=sorted(contours,key=cv2.contourArea,reverse=True)
+
+#the loop extracts the boundary contours of the page
+for c in contours:
+    p=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c,0.02*p,True)
+    
+    if len(approx)==4:
+        target=approx
+        break
+approx=mapper.mapp(target) #find endpoints of the sheet
+pts=np.float32([[0,0],[600,0],[600,800],[0,800]])  #map to 800*800 target window
+
+op=cv2.getPerspectiveTransform(approx,pts)  #get the top or bird eye view effect
+dst=cv2.warpPerspective(orig,op,(600,800))
+
+
+# cv2.imshow("Scanned",dst)
