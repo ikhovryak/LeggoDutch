35deb416d37809fded89cf195b93a170d7458e86
Phan Quan <phanquan_gpu@MacBook-Pro-3.fios-router.home>
2020-08-24 02:03:24 -0400

    Refactor modules and create main pipeline

---
+++ end2end/preprocess_image.py
@@ -0,0 +1,65 @@
+# import the necessary packages
+from transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
+# construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required = True,
+# 	help = "Path to the image to be scanned")
+# args = vars(ap.parse_args())
+
+def preprocess(img_dir):
+    # load the image and compute the ratio of the old height
+    # to the new height, clone it, and resize it
+    image = cv2.imread(img_dir)
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = imutils.resize(image, height = 500)
+    # convert the image to grayscale, blur it, and find edges
+    # in the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    kernel = np.array([[-1,-1,-1], 
+                        [-1, 9,-1],
+                        [-1,-1,-1]])
+    gray = cv2.filter2D(gray, -1, kernel)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    edged = cv2.Canny(gray, 75, 300)
+
+    # find the contours in the edged image, keeping only the
+    # largest ones, and initialize the screen contour
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    
+    try:
+        screenCnt
+    except:
+        raise ValueError("Image has no clear 4 sides")
+    # show the contour (outline) of the piece of paper
+    # cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+
+    # apply the four point transform to obtain a top-down
+    # view of the original image
+    warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+    # convert the warped image to grayscale, then threshold it
+    # to give it that 'black and white' paper effect
+    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+    T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+    warped = (warped > T).astype("uint8") * 255
+
+    return warped
+
